2527ba333be894ec159b0c02186da8943f68a954
Lu Cao <zergmk2@gmail.com>
2019-05-07 18:47:50 +0800

    enable cross domain access by default.

--- trunk/src/app/srs_app_http_stream.cpp
+++ trunk/src/app/srs_app_http_stream.cpp
@@ -502,6 +502,7 @@ int SrsLiveStream::do_serve_http(ISrsHttpResponseWriter* w, ISrsHttpMessage* r)
     srs_assert(entry);
     if (srs_string_ends_with(entry->pattern, ".flv")) {
         w->header()->set_content_type("video/x-flv");
+	w->header()->set("Access-Control-Allow-Origin", "*");
 #ifdef SRS_PERF_FAST_FLV_ENCODER
         bool realtime = _srs_config->get_realtime_enabled(req->vhost);
         if (realtime) {
